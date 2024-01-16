450b745f0e41e13921dd3a88659eb8d3c4c2d1e5
Affaq-Ahmed <aaawan165@gmail.com>
2024-01-16 19:39:23 +0500

    Basic Project Setup
    Install Tailwind CSS and form folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import { Routes } from "./components/Routes";
+import Footer from "./components/Footer";
+import "./global.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [darkTheme, setDarkTheme] = useState(true);
+
+	return (
+		<div className={darkTheme ? "dark" : ""}>
+			<Navbar />
+			<Routes />
+			<Footer />
+		</div>
+	);
+};
 
 export default App;
