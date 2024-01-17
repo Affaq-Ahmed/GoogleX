ba4413dc581a24d504308c37237a0cdc3394e29b
Affaq-Ahmed <aaawan165@gmail.com>
2024-01-17 21:58:47 +0500

    feat: Add Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
-import Routes from "./components/Routes";
+import AppRoutes from "./components/Routes";
 import Footer from "./components/Footer";
 import "./global.css";
 
@@ -12,7 +12,7 @@ const App = () => {
 		<div className={darkTheme ? "dark" : ""}>
 			<div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
 				<Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
-				<Routes />
+				<AppRoutes />
 				<Footer />
 			</div>
 		</div>
