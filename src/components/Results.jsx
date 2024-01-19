221cbdbea47aafb4051ac02b96cb22a02ee20f03
Affaq-Ahmed <aaawan165@gmail.com>
2024-01-19 21:29:59 +0500

    feat: Design the image and NEws section

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -8,13 +8,99 @@ const Results = () => {
 	const location = useLocation();
 	console.log(location.pathname);
 
+	useEffect(() => {
+		if (!searchTerm) {
+			console.log("searchTerm: ", searchTerm);
+			const pathToEndpointMap = {
+				"/search": "/websearch",
+				"/images": "/imagesearch",
+				"/news": "/",
+				"/videos": "/videosearch",
+			};
+
+			const endpoint = pathToEndpointMap[location.pathname];
+
+			if (endpoint) {
+				getResults(endpoint, searchTerm);
+			}
+		}
+	}, [searchTerm, location.pathname]);
+
 	if (isLoading) return <Loading />;
 
 	switch (location.pathname) {
 		case "/search":
-			return "SEARCH";
+			return (
+				<div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+					{results?.map(({ href, title, body }, index) => (
+						<div key={index} className="md:w-2/5 w-full">
+							<a href={href} target="_blank" rel="noreferrer">
+								<p className="text-sm">
+									{href.length > 30 ? href.substring(0, 30) : href}
+								</p>
+								<p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+									{title}
+								</p>
+								<p className="text-sm">
+									{body.length > 60 ? body.substring(0, 60) : body}
+								</p>
+							</a>
+						</div>
+					))}
+				</div>
+			);
 		case "/images":
-			return "IMAGES";
+			return (
+				<div className="flex flex-wrap justify-center items-center">
+					{results?.map(({ image, title, thumbnail, url }, index) => (
+						<a
+							className="sm:p-3 p-5"
+							key={index}
+							href={url}
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								src={image?.src || thumbnail?.src}
+								alt={title}
+								loading="lazy"
+							/>
+							<p className="w-36 break-words text-sm mt-2">{title}</p>
+						</a>
+					))}
+				</div>
+			);
+		case "/news":
+			return (
+				<div className="flex flex-wrap justify-between items-center space-y-6 sm:px-56">
+					{results?.map(({ date, body, title, url, image, source }, index) => (
+						<div key={index} className="md:w-2/5 w-full">
+							<a
+								href={url}
+								target="_blank"
+								rel="noreferrer"
+								className="hover:underline"
+							>
+								<p className="text-sm">
+									{url.length > 30 ? url.substring(0, 30) : url}
+								</p>
+								<p className="text-lg dark:text-blue-300 text-blue-700">
+									{title}
+								</p>
+								<p className="text-sm">
+									{body.length > 60 ? body.substring(0, 60) : body}
+								</p>
+								<div className="flex gap-4">
+									<p className="text-sm">{source}</p>
+									<p className="text-sm">{date}</p>
+								</div>
+							</a>
+						</div>
+					))}
+				</div>
+			);
+		case "/videos":
+			return "VIDEOS";
 		default:
 			return "ERROR";
 	}
--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { createContext, useContext, useState } from "react";
 
 const ResultContext = createContext();
@@ -9,22 +10,63 @@ export const ResultContextProvider = ({ children }) => {
 	const [searchTerm, setSearchTerm] = useState("");
 
 	// /videos, /search, /news, /images
-	const getResults = async (type) => {
+	const getResults = async (type, text) => {
 		setIsLoading(true);
-		try {
-			const response = await fetch(`${baseUrl}${type}`, {
-				method: "GET",
-				headers: {
-					"content-type": "application/json",
-					"X-RapidAPI-Key":
-						"fd3713d204msh44c41bd40ea8fbfp1c41a9jsnebe256f18693",
-					"X-RapidAPI-Host": "google-api31.p.rapidapi.com",
-				},
-			});
+		const ImageData = {
+			text: text,
+			safesearch: "off",
+			region: "wt-wt",
+			color: "",
+			size: "",
+			type_image: "",
+			layout: "",
+			max_results: 100,
+		};
+		const VideoData = {
+			text: text,
+			safesearch: "off",
+			timelimit: "",
+			duration: "",
+			resolution: "",
+			region: "wt-wt",
+			max_results: 50,
+		};
+		const NewsData = {
+			text: text,
+			region: "wt-wt",
+			max_results: 25,
+		};
+		const SearchData = {
+			text: "zarish",
+			safesearch: "off",
+			timelimit: "",
+			region: "wt-wt",
+			max_results: 100,
+		};
 
-			const data = await response.json();
-			console.log(data);
-			setResults(data.value);
+		const url = `${baseUrl}${type}`;
+		const options = {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+				"X-RapidAPI-Key": "fd3713d204msh44c41bd40ea8fbfp1c41a9jsnebe256f18693",
+				"X-RapidAPI-Host": "google-api31.p.rapidapi.com",
+			},
+			data:
+				type === "/imagesearch"
+					? ImageData
+					: type === "/videosearch"
+					? VideoData
+					: type === "/"
+					? NewsData
+					: SearchData,
+		};
+		try {
+			const response = await axios.request(url, options);
+
+			console.log(response.data);
+			setResults(response.data.result);
+			console.log(results);
 		} catch (error) {
 			console.log(error);
 		}
