1f7bc64b0c13b6d60f19030e479d47e07cf350d5
Affaq-Ahmed <aaawan165@gmail.com>
2024-01-21 11:23:07 +0500

    Complete Project

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -29,8 +29,8 @@ const Links = () => {
 					to={to}
 					className={({ isActive }) =>
 						isActive
-							? "text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2"
-							: ""
+							? "text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2 m-2 mb-0"
+							: "m-2 mb-0"
 					}
 				>
 					{text}
